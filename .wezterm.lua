a68ec66c03df9ddfd21922b69856a217324d704f
ivenomweed <ivenomweed+github@gmail.com>
2025-04-12 08:47:23 +0000

    modify wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -33,38 +33,119 @@ config.colors = {
     '#73b8ff',
     '#bfbdb6',
   },
+  tab_bar         = {
+    background = '#020202',
+    active_tab = {
+      bg_color = '#ff8f40',
+      fg_color = '#020202',
+    },
+  },
+
 }
 config.font = wezterm.font {
   family = '0xProto Nerd Font'
 }
 config.harfbuzz_features = { 'ss01' }
-config.enable_tab_bar = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+wezterm.on("update-right-status", function(window, _)
+  local background = "#020202"
+  local foreground = "#bfbdb6"
+
+  if window:leader_is_active() then
+    background = "#ff8f40"
+    foreground = "#020202"
+  end
+  window:set_left_status(wezterm.format {
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = " " .. utf8.char(0xf140c) .. " " },
+  })
+end)
 config.window_close_confirmation = 'NeverPrompt'
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
 config.keys = {
   {
-    key = 't',
-    mods = 'CTRL',
+    key = 'w',
+    mods = 'LEADER',
     action = wezterm.action.SpawnTab 'CurrentPaneDomain',
   },
   {
-    key = 'w',
-    mods = 'CTRL',
+    key = 'x',
+    mods = 'LEADER',
     action = wezterm.action.CloseCurrentTab { confirm = false },
   },
   {
-    key = '/',
-    mods = 'CTRL',
+    key = 'p',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    key = 'n',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = '\\',
+    mods = 'LEADER',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
   {
-    key = '?',
-    mods = 'CTRL|SHIFT',
+    key = '-',
+    mods = 'LEADER',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
   {
-    key = '.',
-    mods = 'CTRL',
+    key = 'r',
+    mods = 'LEADER',
     action = wezterm.action.CloseCurrentPane { confirm = false },
   },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection "Left",
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection "Down",
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection "Up",
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection "Right",
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'LEADER',
+    action = wezterm.action.AdjustPaneSize { "Left", 5 },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'LEADER',
+    action = wezterm.action.AdjustPaneSize { "Right", 5 },
+  },
+  {
+    key = 'UpArrow',
+    mods = 'LEADER',
+    action = wezterm.action.AdjustPaneSize { "Up", 5 },
+  },
+  {
+    key = 'DownArrow',
+    mods = 'LEADER',
+    action = wezterm.action.AdjustPaneSize { "Down", 5 },
+  },
 }
+for i = 0, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = wezterm.action.ActivateTab(i)
+  })
+end
 return config
